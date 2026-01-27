8e3e6787c288bf879246c7686f6c8e0d28dafe54
Charlie <cchan_03@yahoo.ca>
2026-01-27 15:34:50 -0500

    changed wordings and design

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import './App.css'
 import getComputerInput from './getComputerInput';
 
 function App() {
   const [ playerInput, setPlayerInput ] = useState('');
   const [ computerInput, setComputerInput ] = useState('');
-  const [ result, setResult ] = useState('')
+  const [ result, setResult ] = useState('');
+  const [ showResult, setShowResult ] = useState(false);
   const beats = {
       rock: ['scissors', 'lizard'],
       scissors: ['paper', 'lizard'],
@@ -16,29 +17,31 @@ function App() {
 
   const handleChange = (value) => {
     setPlayerInput(value);
+    setShowResult(false);
   }
 
   const handleClick = () => {
     let compInput = getComputerInput();
-    setComputerInput(compInput);
+    setComputerInput(compInput.charAt(0).toUpperCase() + compInput.slice(1));
     setResult(() => {
       if (compInput === playerInput) {
-        return "It is a draw"
+        return "Draw - great minds think alike."
       } else {
-        let res = (beats[playerInput].includes(compInput) ? 'Player' : 'Computer'); 
-        return `The winner is ${res}`;
+        let res = (beats[playerInput].includes(compInput) ? 'You take the win!' : 'The computer takes the win.'); 
+        return res;
       }
-    })
+    });
+    setShowResult(true);
   }
 
   return ( 
     <div className='main-container'>
-      <div className='container'>
+      <div className='wrapper'>
         <div className='title-container'>
-          <div className='title'>Rock, Paper, Scissors, Lizard and Spock</div>
+          <div className='title'>Rock Paper Scissors Lizard Spock</div>
         </div>
-        <div className='input-container'>
-          <label for='choice'>Pick a choice:</label>
+        <div className='container'>
+          <label htmlFor='choice'>Make your selection</label>
           <select 
             id='choice'
             name='choice'
@@ -55,11 +58,21 @@ function App() {
           </select>
         </div>
 
-        <button className='btn' onClick={() => handleClick()}>Generate Computer Input</button>
+        <button className='btn' onClick={() => handleClick()}>Play Round</button>
         <div className='container'>
-          <div className='computer'>Computer Choice: {computerInput}</div>
+          <div className='computer'>Computer's Move': &nbsp;&nbsp;{computerInput}</div>
         </div>
-          <div className='winner'>Result: {result}</div>
+          <div className={showResult ? 'winner show-result' : 'winner'} >{result}</div>
+      </div>
+      <div className='rule-container'>
+        <div className='rule-title'>Rules:</div>
+        <div className='info-container'>
+          <div className="rules"><strong>Rock</strong> defeats Scissors and Lizard</div>
+          <div className="rules"><strong>Paper</strong> defeats Rock and Spock</div>
+          <div className="rules"><strong>Scissors</strong> defeats Paper and Lizard</div>
+          <div className="rules"><strong>Lizards</strong> defeats Paper and Spock</div>
+          <div className="rules"><strong>Spock</strong> defeats Scissors and Rock</div>
+        </div>
       </div>
     </div>
   )
